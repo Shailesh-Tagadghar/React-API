137a03fd92b0d347df2904dd6aceb465497e6fe7
Shailesh-Tagadghar <shaileshtagadghar13@gmail.com>
2023-10-14 14:59:10 +0530

    Fetching Data with Students API from express file

--- reactapi/src/Student.js
+++ reactapi/src/Student.js
@@ -1,30 +1,49 @@
 import React, { useEffect } from 'react'
-import  {useState} from 'react'
+import { useState } from 'react'
 
 export default function Student() {
     // var x =5;
-    const [data,setData] = useState([]);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        setData([{"name":"shailesh", "age":21, "city":"shahad"}]);
-    },[])
-  return (
-    <>
-    {JSON.stringify(data)}
-    <table border={1}>
-        <thead>
-            <th>Sr. No</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>City</th>
-        </thead>
-        <tbody>
-            <td>1</td>
-            <td>Shailesh</td>
-            <td>21</td>
-            <td>Shahad</td>
-        </tbody>
-    </table>
-    </>
-  )
+        // setData([{ "name": "shailesh", "age": 21, "city": "shahad" },
+        // { "name": "Monika", "age": 20, "city": "Panvel" }]);
+
+        fetch("http://localhost:8989/students")
+        .then(res => res.json())
+        .then(result => {
+            console.log(result);
+            console.table(result)
+            setData(result)
+        })
+    }, [])
+    return (
+        <>
+            {/* {JSON.stringify(data)} */}
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>Sr. No</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>City</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((x, i) => {
+                            return <tr>
+                                <td>{i+1}</td>
+                                <td>{x.name}</td>
+                                <td>{x.age}</td>
+                                <td>{x.city}</td>
+                                <td><button>Delete</button></td>
+                            </tr>;
+                        })
+                    }
+                </tbody>
+            </table>
+        </>
+    )
 }
